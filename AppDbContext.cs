a5ee4613171410fe82f95f6ec49edad7d4a35c64
Jogger <beltchikov@gmx.de>
2020-10-14 09:06:32 +0200

    Connection string added

---
+++ AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using AspNetCore31Tc.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore31Tc
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
